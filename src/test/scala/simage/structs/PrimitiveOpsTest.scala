d4e9b1110037b25d7b2c9932c1bc23438c78eb98
Eivind Barstad Waaler <eivindw@gmail.com>
2009-10-04 22:01:50 +0200

    3x speed improvement

--- src/test/scala/simage/structs/PrimitiveOpsTest.scala
+++ src/test/scala/simage/structs/PrimitiveOpsTest.scala
@@ -12,39 +12,73 @@ class PrimitiveOpsTest extends Suite {
 
       def apply(i: Int, j: Int) = arr(i + j * nCols)
 
-      def hasPoint(x: Int, y: Int) = x >= 0 && x < nCols && y >= 0 && y < nRows
-
-      def seValues(se: ArrayMatrix, row: Int, col: Int) = {
+      def seOp2(se: ArrayMatrix, op: (Seq[Int]) => Int) = {
          val w = se.nCols / 2
          val h = se.nRows / 2
-         val p = row + col * nCols
 
-         Array(
-            arr(p - nCols - 1), arr(p - nCols), arr(p - nCols + 1),
-            arr(p - 1), arr(p), arr(p + 1),
-            arr(p + nCols - 1), arr(p + nCols), arr(p + nCols + 1)
-         )
-      }
-
-      def seOp(se: ArrayMatrix, op: (Seq[Int]) => Int) = {
-         def pointOp(row: Int, col: Int) = {
-            val w = se.nCols / 2
-            val h = se.nRows / 2
-            val tmp = for {
+         def seValues(row: Int, col: Int) = {
+            for {
                x <- -w to w
                cx = col + x
                y <- -h to h
                ry = row + y
-               if(hasPoint(cx, ry))
+               if(cx >= 0 && cx < nCols && ry >= 0 && ry < nRows)
             } yield {
-               apply(cx, ry) * se(w + x, h + y)
+               arr(cx + ry * nCols)
             }
-            op(tmp)
          }
          val range = for(i <- 0 until nRows; j <- 0 until nCols) yield {
-            pointOp(i, j)
+            op(seValues(i, j))
          }
          new ArrayMatrix(nCols, range.toArray)
+      }
+
+      def seOp3(se: ArrayMatrix, op: (Seq[Int]) => Int) = {
+         val w = se.nCols / 2
+         val h = se.nRows / 2
+
+         def seValues(row: Int, col: Int) = {
+            for {
+               x <- -w to w
+               cx = col + x
+               y <- -h to h
+               ry = row + y
+               if(cx >= 0 && cx < nCols && ry >= 0 && ry < nRows)
+            } yield {
+               arr(cx + ry * nCols)
+            }
+         }
+         val newArr = Array.make(arr.size, 0)
+         for(j <- 0 until nCols; i <- 0 until nRows) {
+            println("Setting: " + (i + j * nCols) + " to: " + op(seValues(j, i)))
+            newArr(i + j * nCols) = op(seValues(j, i))
+         }
+         new ArrayMatrix(nCols, newArr)
+      }
+
+      def seOp(se: ArrayMatrix, op: (Seq[Int]) => Int) = {
+         val w = se.nCols / 2
+         val h = se.nRows / 2
+
+         def seValues(row: Int, col: Int) = {
+         }
+         val newArr = Array.make(arr.size, 0)
+         for(j <- 0 until nCols; i <- 0 until nRows) {
+            var points: List[Int] = Nil
+            for {
+               x <- -w to w
+               cx = i + x
+               y <- -h to h
+               ry = j + y
+               if(cx >= 0 && cx < nCols && ry >= 0 && ry < nRows)
+            } {
+               points = arr(cx + ry * nCols) :: points
+            }
+            val avg = op(points)
+            println("Setting: " + (i + j * nCols) + " to: " + avg)
+            newArr(i + j * nCols) = avg
+         }
+         new ArrayMatrix(nCols, newArr)
       }
 
       override def toString = {
@@ -52,26 +86,17 @@ class PrimitiveOpsTest extends Suite {
       }
    }
 
-   val arr = (1 to 1000000).toArray
-   val arrMtx = new ArrayMatrix(1000, arr)
+   val arr = (1 to 9).toArray
+   val arrMtx = new ArrayMatrix(3, arr)
    val se = new ArrayMatrix(3, Array(1, 1, 1, 1, 1, 1, 1, 1, 1))
 
-   /*def testAvgOperation {
+   def testAvgOperation {
       val res = arrMtx.seOp(se, (seq) => seq.reduceLeft(_ + _) / seq.size)
       println(res)
-   }*/
+   }
 
-   def testGetSeValues {
-      val mtx = new ArrayMatrix(3, Array(1, 2, 3, 4, 5, 6, 7, 8, 9))
-      val e1 = Array(1, 2, 4, 5)
-      val e2 = Array(1, 2, 3, 4, 5, 6, 7, 8, 9)
-      val e3 = Array(5, 6, 8, 9)
-
-      val v1 = mtx.seValues(se, 0, 0)
-      assert(e1 deepEquals v1)
-      val v2 = mtx.seValues(se, 1, 1)
-      assert(e2 deepEquals v2)
-      val v3 = mtx.seValues(se, 2, 2)
-      assert(e3 deepEquals v3)
+   def testAvgOperation2 {
+      val res = arrMtx.seOp2(se, (seq) => seq.reduceLeft(_ + _) / seq.size)
+      println(res)
    }
 }
